95d2242b8de5b7568f5599c299f9902e50aa5a21
Guilherme de Almeida Martins <54013549+Guil-Baka@users.noreply.github.com>
2022-12-03 15:53:16 -0300

    Inicio da idéia geral da ATV8

---
+++ C/Resources/Atividades/ATV8.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define tamanhoVetor 10
+
+int checkConteudoByOrderOfReference(int vetConteudo[],int vetIndex[])
+{
+  int result = 0;
+  int i = vetIndex[0];
+  while (vetIndex[i]<tamanhoVetor)
+  {
+
+    // if (vetConteudo[i] > )
+    // {
+
+    // }
+    result = vetConteudo[vetIndex[i]];
+    printf("%d\n",result);
+    i++;
+  }
+  
+}
+
+int main(int argc, char const *argv[])
+{
+  int vetConteudo[tamanhoVetor]=     {1,2,3,4,5,6,7,8,9,10};
+  int vetIndex[tamanhoVetor]=        {0,1,2,3,4,5,6,7,8,9};
+  checkConteudoByOrderOfReference(vetConteudo,vetIndex);
+  return 0;
+}
